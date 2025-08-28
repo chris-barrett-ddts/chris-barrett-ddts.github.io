752683a28e30dd0d20b98bb42493a70b16c72d22
chris-barrett-ddts <chris.barrett@defra.gov.uk>
2025-08-28 11:00:32 +0100

    Update eleventy
    moved to the latest x gov plugin and needed to do some reconfguration.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,6 @@
-const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
+import { govukEleventyPlugin } from '@x-govuk/govuk-eleventy-plugin'
 
-module.exports = function(eleventyConfig) {
-  // Register the plugin
+export default function(eleventyConfig) {
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     header: {
         logotype: {
@@ -15,7 +14,8 @@ module.exports = function(eleventyConfig) {
         copyright: {
             text: '© Sustainable by design'
           }
-    }
+    },
+    stylesheets: ['/assets/styles.css', '/assets/application.css' ],
 });
 
   return {
